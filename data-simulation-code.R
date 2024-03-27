92ec029442cad4c8d05dee12ba4dc8e68e6791d1
josefinabernardo <113699978+josefinabernardo@users.noreply.github.com>
2024-03-27 11:27:04 +0100

    Update data-simulation-code.R
    Creating empty data frames to store results.

--- data-simulation-code.R
+++ data-simulation-code.R
@@ -28,11 +28,46 @@ dolan_simulation_function <- function(nrep = 500, # Number of repetitions
              a = sqrt(c(.5, .6)), # Additive genetic path coefficient
              c = sqrt(c(.2, .1)), # Shared environmental path coefficient
              e = sqrt(c(.3, .3)), # Unique environmental path coefficient
-             ct = sqrt(c(0, .01, .0025)), # Parent genotype to child phenotype
-             si = sqrt(c(0, .01, .0025)) # Twin 1 genotype to twin 2 phenotype
+             ct = sqrt(c(0, .01, .0025)), # Cultural Transmission - Parent genotype to child phenotype
+             si = sqrt(c(0, .01, .0025)), # Sibling Interaction - Sibling 1 genotype to sibling 2 phenotype
+             nloci = 100, # Number of diallelic loci
+             npgsloci = c(2, 5, 10, 15) # Number of loci comprising the PGS
     ){
-      nset = length(a) * length(c) * length(e) * length(ct) * length(si)
-      nset
+  
+      #Create all possible parameter combinations
+      param_combinations <- expand.grid(a = a, c = c, e = e, ct = ct, si = si)
+      
+      # Only retain distinct combinations where A + C + E = 1
+      filtered_combinations <- param_combinations %>%
+        filter(a^2 + c^2 + e^2 == 1) %>%
+        distinct()
+      
+      # Number of settings we iterate through
+      n_set = nrow(filtered_combinations)
+      
+      # R2 of the polygenic scores
+      p_pgs = npgsloci/nloci
+      
+      # Initiate counters
+      counter_within = 0  # counts sets within PGS setting
+      counter_overall = 0 # counts sets overall
+      
+      # Determine numner of rows for data frames
+      n_rows = n_set * length(p_pgs)
+      
+      # Pre-allocate data frames with the appropriate dimensions
+      final_estimates <- data.frame(matrix(NA, nrow = n_rows, ncol = 19))
+      final_power <- data.frame(matrix(NA, nrow = n_rows, ncol = 19))
+      
+      # Create 
+      setkeep = matrix(NA, n_set, 30)   # to keep settings 
+      reskeep = matrix(NA, n_set, 30)   # to keep results each data set
+      mxkeep = matrix(NA, n_set, 30) # openmx results
+      
+      colnames(setkeep) = c(c('nmz','ndz','a','c','e','g','b','x','prs','A'), rep(NA, 20))
+      
+      # Print number of settings to the user
+      print(paste('The factorial design has', n_set, 'setting(s).'))
 }
 
 dolan_simulation_function(a = sqrt(c(.5, .6)), c = sqrt(c(.2, .1)),
@@ -40,5 +75,11 @@ dolan_simulation_function(a = sqrt(c(.5, .6)), c = sqrt(c(.2, .1)),
 
 
 # ISSUES
+
 # Why do we round down the NCP?
 # Why is there another type of sibling interaction model?
+
+# Do some lintering and code-formatting.
+# Why does the function say it's recursive when I start it without arguments?
+# Have a closer look at the number of rows and columns for final_power & final_estimates.
+# Make sure I can get out mx and gee regression results at the same time
