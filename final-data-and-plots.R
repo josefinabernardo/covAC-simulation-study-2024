fce5e757f67d1c4b6f78f9688741928401246405
josefinabernardo <113699978+josefinabernardo@users.noreply.github.com>
2024-09-06 16:55:08 +0200

    Saved plots

--- final-data-and-plots.R
+++ final-data-and-plots.R
@@ -137,143 +137,36 @@ plot4 <- ggplot(data_noSI_long, aes(x = CT, y = value, color = variable)) +
   scale_color_viridis_d(name = "variable")
 
 
-# Plot 5 - new version
-plot5_mx_data <- gnomesims::gnome_mx_simulation(ct = seq(0,.1,.02), si = seq(0,.1,.02),
-                                             nloci = 100,
-                                             npgsloci = c(2, 5, 10, 15))
+# Plot 5
+p1_gee_data <- gee_power %>%
+  filter(g == 0) %>%
+  dplyr::select("b", "p1", "PGS") %>%
+  mutate(Variable = "CT", Sample = "MZ & DZ") %>%
+  rename(Confounder = b, Power = p1)
 
-plot5_gee_data <- gnomesims::gnome_gee_simulation(ct = seq(0,.1,.02), si = seq(0,.1,.02),
-                                            nloci = 100,
-                                            npgsloci = c(2, 5, 10, 15), cmethod = "exchangeable")
+p2_gee_data <- gee_power %>%
+  filter(b == 0) %>%
+  dplyr::select("g", "p2", 'PGS') %>%
+  mutate(Variable = "SI", Sample = "MZ & DZ") %>%
+  rename(Confounder = g, Power = p2)
 
-plot5_mx_power <- plot5_mx_data$power
-plot5_mx_estimates <- plot5_mx_data$params
-
-plot5_gee_power <- plot5_gee_data$power
-plot5_gee_estimates <- plot5_gee_data$params
-
-write.csv(plot5_mx_power, "plot5_mx_power_estimates.csv", row.names = TRUE)
-write.csv(plot5_mx_estimates, "plot5_mx_parameter_estimates.csv", row.names = TRUE)
-write.csv(plot5_gee_power, "plot5_gee_power_estimates.csv", row.names = TRUE)
-write.csv(plot5_gee_estimates, "plot5_gee_parameter_estimates.csv", row.names = TRUE)
-
-mx_power5 <- read.csv("plot5_mx_power_estimates.csv")
-gee_power5 <- read.csv("plot5_gee_power_estimates.csv")
-
-p3_gee_data <- gee_power5 %>%
-#  dplyr::filter(PGS == .10) %>%
-  dplyr::select("g", "b", "p3", "PGS") %>%
-  mutate(Variable = "CT") %>%
-  rename("True Parameter" = "g", "Other covAC Source" = "b", "Power" = "p3")
-
-p4_gee_data <- gee_power5 %>%
-#  dplyr::filter(PGS == .10) %>%
-  dplyr::select("b", "g", "p4", "PGS") %>%
-  mutate(Variable = "SI") %>%
-  rename("True Parameter" = "b", "Other covAC Source" = "g", "Power" = "p4")
-
-gee_combi_data <- rbind(p3_gee_data, p4_gee_data) %>%
-  mutate(Method = "Gee",
-         Label = paste0(Method, " (Other Source: ", `Other covAC Source`, ")"))
-
-p3_mx_data <- mx_power5 %>%
-#  dplyr::filter(PGS == .10) %>%
-  dplyr::select("g", "b", "p3", "PGS") %>%
-  mutate(Variable = "CT") %>%
-  rename("True Parameter" = "g", "Other covAC Source" = "b", "Power" = "p3")
-
-p4_mx_data <- mx_power5 %>%
-#  dplyr::filter(PGS == .10) %>%
-  dplyr::select("b", "g", "p4", "PGS") %>%
-  mutate(Variable = "SI") %>%
-  rename("True Parameter" = "b", "Other covAC Source" = "g", "Power" = "p4")
-
-mx_combi_data <- rbind(p3_mx_data, p4_mx_data) %>%
-  mutate(Method = "OpenMx",
-         Label = paste0(Method, " (Other Source: ", `Other covAC Source`, ")"))
-
-full_combi_data <- rbind(mx_combi_data, gee_combi_data) %>%
+gee_mzdz_data <- rbind(p1_gee_data, p2_gee_data) %>%
   mutate(PGS_percent = factor(scales::percent(PGS), levels = c("2%", "5%", "10%", "15%")))
 
-ct_data <- full_combi_data %>%
-  dplyr::filter(Variable == "CT", `Other covAC Source` %in% c(0, .05, .1))
+gee_mzdz_data$Method <- c("Gee")
+mx_mzdz_data$Method <- c("OpenMx")
 
+full_mzdz_data <- rbind(gee_mzdz_data, mx_mzdz_data)
 
-si_data <- full_combi_data %>%
-  dplyr::filter(Variable == "SI", `Other covAC Source` %in% c(0, .05, .1))
+plot5 <- ggplot(data = full_mzdz_data, mapping = aes(x = Confounder, y = Power, color = Variable, linetype = Method)) +
+geom_line(linewidth = 0.8) +
+geom_point() +
+facet_wrap(~PGS_percent) +
+scale_linetype_manual(values = c("Gee" = "dotted", "OpenMx" = "solid")) +
+jtools::theme_apa() +
+theme(text = element_text(family = "serif")) +
+scale_color_viridis_d(name = "variable")
 
-ggplot(data = ct_data, mapping = aes(x = `True Parameter`, y = Power, group = as.factor(Label), color = as.factor(`Other covAC Source`))) +
-  geom_point() +
-  geom_line(aes(linetype = Method)) +
-  facet_wrap(~PGS_percent) +
-  theme(text = element_text(family = "serif")) +
-  scale_linetype_manual(values = c("OpenMx" = "solid", "Gee" = "dotted")) +
-  scale_color_viridis_d(name = "True Value Sibling Interaction (b)") +
-  labs(x = "True Value Cultural Transmission (g)", y = "Power") +
-  theme_light() +
-  theme(text = element_text(family = "serif"))
-
-ggplot(data = si_data, mapping = aes(x = `True Parameter`, y = Power, group = as.factor(Label), color = as.factor(`Other covAC Source`))) +
-  geom_point() +
-  geom_line(aes(linetype = Method)) +
-  facet_wrap(~PGS_percent) +
-  theme(text = element_text(family = "serif")) +
-  scale_linetype_manual(values = c("OpenMx" = "solid", "Gee" = "dotted")) +
-  scale_color_viridis_d(name = "True Value Cultural Transmission (g)") +
-  labs(x = "True Value Sibling Interaction (b)", y = "Power") +
-  theme_light() +
-  theme(text = element_text(family = "serif"))
-
-# Select a single PGS Level
-full_combi_data %>%
-  mutate(Label = paste(Method, `Other covAC Source`),
-         Variable = recode(Variable, "CT" = "Cultural Transmission (CT)", "SI" = "Sibling Interaction (SI)")) %>%
-  filter(PGS == .1, `Other covAC Source` %in% c(0, .05, .1)) %>%
-ggplot(data = ., mapping = aes(x = `True Parameter`, y = Power,
-                               color = as.factor(`Other covAC Source`), linetype = Method,
-                               group = Label)) +
-  geom_point() +
-  geom_line() +
-  facet_wrap(~Variable) +
-  theme(text = element_text(family = "serif")) +
-  scale_color_viridis_d(name = "Other Source of covAC") +
-  scale_linetype_manual(values = c("OpenMx" = "solid", "Gee" = "dotted")) +
-  labs(x = "True Parameter (g for CT, b for SI)", y = "Power") +
-  theme_light() +
-  theme(text = element_text(family = "serif"))
-
-
-full_combi_data %>%
-  mutate(Label = paste(Method, `Other covAC Source`, PGS_percent),
-         Variable = recode(Variable, "CT" = "Cultural Transmission (CT)", "SI" = "Sibling Interaction (SI)")) %>%
-  filter(PGS_percent %in% c("2%", "10%", "15%"),
-         `Other covAC Source` %in% c(0, .05, .1)) %>%
-  ggplot(aes(x = `True Parameter`, y = Power,
-             color = as.factor(`Other covAC Source`), linetype = Method,
-             group = Label)) +
-  geom_point() +
-  geom_line() +
-  facet_grid(PGS_percent ~ Variable) +  # Custom facet labels
-  scale_color_viridis_d(name = "Other Source of covAC") +
-  scale_linetype_manual(values = c("OpenMx" = "solid", "Gee" = "dotted"), name = "Method") +  # Custom legend title
-  labs(x = "True Parameter (g for CT, b for SI)", y = "Power") +
-  theme_light() +
-  theme(text = element_text(family = "serif"))
-
-# Combine CT and SI into one plot
-full_combi_data %>%
-  filter(`Other covAC Source` %in% c(.1)) %>%
-  mutate(Label = paste(Method, Variable)) %>%
-  ggplot(data = ., mapping = aes(x = `True Parameter`, y = Power,
-                                 group = as.factor(Label), linetype = Method,
-                                 colour = Variable)) +
-  geom_point() +
-  geom_line() +
-  facet_wrap(~PGS_percent) +
-  jtools::theme_apa() +
-  theme(text = element_text(family = "serif")) +
-  scale_color_viridis_d(name = "Variable") +
-  scale_linetype_manual(values = c("OpenMx" = "solid", "Gee" = "dotted"))
 
 # Set width and height in inches (1 inch = 25.4 mm)
 width <- 200 / 25.4  # Convert 200 mm to inches
@@ -286,3 +179,18 @@ ggsave("plot3.pdf", plot = plot3, device = "pdf", width = width, height = height
 ggsave("plot4.pdf", plot = plot4, device = "pdf", width = width, height = height)
 ggsave("plot5.pdf", plot = plot5, device = "pdf", width = width, height = height)
 
+
+# Calculate PGS Predictive Power
+gnome_pgs_pred <- function(a, c, e, g, b, pgs_prop) {
+
+  # Phenotypic covariances for MZ and DZ
+  smz = 2 * (g + a/2 + b/2)**2 + (a/2 + b/2) * a + (a/2 + b/2) * b + c**2 + e**2
+  sdz = 2 * (g + a/2 + b/2)**2 + a**2/2 + b**2/2 + c**2 + e**2
+
+  # Variance Increase
+  effect_mz <- smz - 1
+  effect_dz <- sdz - 1
+
+  effect = list(mz = effect_mz, dz = effect_dz)
+  return(effect)
+}
