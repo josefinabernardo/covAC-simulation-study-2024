f3c6d60c1a5c676e410d5e695c8cb306e024d545
josefinabernardo <113699978+josefinabernardo@users.noreply.github.com>
2024-09-09 18:59:25 +0200

    Tables, gee results

--- final-data-and-plots.R
+++ final-data-and-plots.R
@@ -192,16 +192,98 @@ ggplot(data = full_method_data, mapping = aes(x = Confounder, y = Power, color =
 gnome_pgs_pred <- function(a, c, e, g, b, pgs_prop, vA = 1, vC = 1, vE = 1) {
 
   # Phenotypic variances
-  smz = 2 * (g + a/2 + b/2)**2 + (a/2 + b/2) * a + (a/2 + b/2) * b + c**2 + e**2
-  sdz = 2 * (g + a/2 + b/2)**2 + a**2/2 + b**2/2 + c**2 + e**2
+  var_mz = 2*(g+a/2+b/2)**2*vA+(a*vA/2+b*vA/2)*a+(a*vA/2+b*vA/2)*b+c**2*vC+e**2*vE
+  var_dz = 2*(g+a/2+b/2)**2*vA+a**2*vA/2+b**2*vA/2+c**2*vC+e**2*vE
 
-  Ph_mz = 2*(g+a/2+b/2)**2 *vA +a**2*vA /2+b**2*vA /2+c**2*vC+e**2*vE
-  Ph_dz = 2*(g+a/2+b/2)**2 *vA +a**2*vA /2+b**2*vA /2+c**2*vC+e**2*vE
+  # PGS variance
+  vA1 = (1-pgs_prop)*vA
+  vA2  = pgs_prop*vA
 
-  # Variance Increase
-  effect_mz <- smz - 1
-  effect_dz <- sdz - 1
+  var_pgs = 2*(g+a/2+b/2)**2 *vA2 +a**2*vA2 /2+b**2*vA2 /2
+
+  effect_mz <- var_pgs/var_mz
+  effect_dz <- var_pgs/var_dz
 
   effect = list(mz = effect_mz, dz = effect_dz)
   return(effect)
 }
+
+# Add these variable to
+paper_power %>%
+  filter(b == 0, g == 0) %>%
+  mutate(PGS_effect_MZ = gnome_pgs_pred(a, c, e, g, b, pgs_prop = PGS)$mz,
+         PGS_effect_DZ = gnome_pgs_pred(a, c, e, g, b, pgs_prop = PGS)$dz)
+
+paper_power %>%
+  filter(PGS == 0.10) %>%
+  mutate(PGS_effect_MZ = gnome_pgs_pred(a, c, e, g, b, pgs_prop = PGS)$mz,
+         PGS_effect_DZ = gnome_pgs_pred(a, c, e, g, b, pgs_prop = PGS)$dz)
+
+appendix_table5 <- gnomesims::gnome_mx_simulation(ct = seq(0,.1,.05), si = seq(0,.1,.05),
+                               nloci = 100,
+                               npgsloci = 10)
+
+appendix_table5$power %>%
+  mutate(PGS_effect_MZ = scales::percent(round(gnome_pgs_pred(a, c, e, g, b, pgs_prop = PGS)$mz, 4)),
+         PGS_effect_DZ = scales::percent(round(gnome_pgs_pred(a, c, e, g, b, pgs_prop = PGS)$dz, 4))) %>%
+  select(g, b, PGS_effect_MZ, PGS_effect_DZ)
+
+appendix_table5$power %>%
+  mutate(Smz = scales::percent(round(Smz,4)),
+         Sdz = scales::percent(round(Sdz,4)),
+         g = round(g, 3), b = round(b, 3)) %>%
+  select(g, b, p1:p4, Smz, Sdz)
+
+appendix_table5$params %>%
+  mutate(Smz = scales::percent(round(Smz,4)),
+         Sdz = scales::percent(round(Sdz,4)),
+         g = round(g, 3), b = round(b, 3)) %>%
+  select(g, b, e1:e4, Smz, Sdz)
+
+# Final tables gee
+gee_appendix_table_data <- gnomesims::gnome_gee_simulation(ct = seq(0,.1,.05), si = seq(0,.1,.05),
+                                            nloci = 100,
+                                            npgsloci = c(2, 5, 10, 15), cmethod = "exchangeable")
+
+gee_appendix_table3 <- gee_appendix_table_data$power %>%
+  mutate(Smz = scales::percent(Smz, accuracy = 1),
+         Sdz = scales::percent(Sdz, accuracy = 1),
+         g = round(g, 2),
+         b = round(b, 2),
+         A = round(a^2, 1),
+         C = round(c^2, 1),
+         E = round(e^2, 1),
+         p1 = round(p1, 2),
+         p2 = round(p2, 2),
+         p3 = round(p3, 2),
+         p4 = round(p4, 2),
+         p5 = round(p5, 2),
+         p6 = round(p6, 2),
+         p7 = round(p7, 2),
+         p8 = round(p8, 2)) %>%
+  select(A, C, E, g, b, PGS, p1:p8, Smz, Sdz)
+
+
+gee_appendix_table4 <- gee_appendix_table_data$params %>%
+  mutate(Smz = scales::percent(Smz, accuracy = 1),
+         Sdz = scales::percent(Sdz, accuracy = 1),
+         g = round(g, 2),
+         b = round(b, 2),
+         A = round(a^2, 1),
+         C = round(c^2, 1),
+         E = round(e^2, 1),
+         e1 = round(e1, 2),
+         e2 = round(e2, 2),
+         e3 = round(e3, 2),
+         e4 = round(e4, 2),
+         e5 = round(e5, 2),
+         e6 = round(e6, 2),
+         e7 = round(e7, 2),
+         e8 = round(e8, 2)) %>%
+  select(A, C, E, g, b, PGS, e1:e8, Smz, Sdz)
+
+# Convert gee_appendix_table3 to CSV format without index and print
+cat(paste0(capture.output(write.csv(gee_appendix_table3, row.names = FALSE, quote = FALSE)), collapse = "\n"))
+
+# Convert gee_appendix_table4 to CSV format without index and print
+cat(paste0(capture.output(write.csv(gee_appendix_table4, row.names = FALSE, quote = FALSE)), collapse = "\n"))
