4e3118376685db13b90cc6aa40bfdd960dd6e764
josefinabernardo <113699978+josefinabernardo@users.noreply.github.com>
2024-10-09 10:22:18 +0200

    Appendix De Zeeuw Paper

--- appendix-analyses.R
+++ appendix-analyses.R
@@ -3,7 +3,22 @@ devtools::install_github("josefinabernardo/gnomesims", force = TRUE)
 library(gnomesims)
 
 # Results Sibling Mean
-gnomesims::gnome_mx_simulation(a = sqrt(.74), c = sqrt(.08), e = sqrt(.18),
-                               nmz = 2451, ndz = 4569, nloci = 100,
-                               npgsloci = 12, ct = seq(.1,.13,.01), si = seq(.1,.13,.01))
+dezeeuw_data <- gnomesims::gnome_mx_simulation(a = sqrt(.74), c = sqrt(.08),
+                      e = sqrt(.18), nmz = 2451, ndz = 4569, nloci = 100,
+                      npgsloci = 12, ct = seq(0,.12,.01), si = seq(0,.12,.01))
+
+# Create seperate data sets for processing
+dezeeuw_power <- dezeeuw_data$power
+dezeeuw_estimates <- dezeeuw_data$params
+
+# Write to .csv files
+write.csv(dezeeuw_estimates, file = "dezeeuw_estimates.csv", row.names = TRUE)
+write.csv(dezeeuw_power, file = "dezeeuw_power.csv", row.names = TRUE)
+
+dezeeuw_power <- read.csv("dezeeuw_power.csv")
+
+dezeeuw_power %>%
+  filter(g == b) %>%
+  select(g, b, p1, p2, p3, p4, Smz, Sdz)
+
 
