37f34e7acf99073fefb49bc415f22f8c0a02a374
Josefina Bernardo <josefinabernardo99@gmail.com>
2025-10-10 09:27:50 +0200

    Updated file structure

--- create-datasets-final-version.R
+++ create-datasets-final-version.R
@@ -1,9 +1,7 @@
 # Install all packages
 
-# install.packages("~/GitHub/gnomesims_0.0.0.9000.tar.gz", repos = NULL, type = "source")
-devtools::install_github("josefinabernardo/gnomesims", force = TRUE)
+devtools::install_github("josefinabernardo/gnomesims")
 library(gnomesims)
-library(OpenMx)
 library(tidyverse)
 
 # Run function for detailed lots in running text
@@ -15,6 +13,10 @@ gee_data <- gnomesims::gnome_gee_simulation(ct = seq(0,.1,.02), si = seq(0,.1,.0
                                           nloci = 100,
                                         npgsloci = c(2, 5, 10, 15), cmethod = "exchangeable")
 
+dz_data <- gnomesims::gnome_mx_simulation(ct = seq(0,.1,.02), si = seq(0,.1,.02),
+                                          nloci = 100, ndz = 8000,
+                                          npgsloci = c(2, 5, 10, 15))
+
 # Create seperate data sets for processing
 paper_power <- paper_data$power
 paper_estimates <- paper_data$params
@@ -22,12 +24,18 @@ paper_estimates <- paper_data$params
 gee_power <- gee_data$power
 gee_estimates <- gee_data$params
 
+dz_power <- dz_data$power
+dz_estimates <- dz_data$params
+
 # Write to .csv files
 write.csv(paper_estimates, file = "paper_mx_estimates.csv", row.names = TRUE)
 write.csv(paper_power, file = "paper_mx_power.csv", row.names = TRUE)
 
 write.csv(gee_estimates, file = "paper_gee_estimates.csv", row.names = TRUE)
 write.csv(gee_power, file = "paper_gee_power.csv", row.names = TRUE)
+
+write.csv(dz_estimates, file = "paper_dz_estimates.csv", row.names = TRUE)
+write.csv(dz_power, file = "paper_dz_power.csv", row.names = TRUE)
 
 # Varying sample size
 sample_sizes <- c(100, seq(10000, 50000, 10000))
--- plots-submission.R
+++ plots-submission.R
@@ -88,23 +88,17 @@ plot2_int_1 <- plot_grid(part1, part2, ncol = 2, align = "hv")
 plot2_int_2 <- plot_grid(part3, part4, ncol = 2, align = "hv")
 plot2_int <- plot_grid(part1, part2, part3, part4, ncol = 2, align = "hv")
 
-plot2_1 <- ggdraw() +
+Figure2 <- ggdraw() +
   draw_plot(plot2_int_1, x = 0.05, y = 0.05, width = 0.95, height = 0.95) +
   draw_label("Cultural Transmission", x = 0.5, y = 0, vjust = -0.5, fontfamily = "CMU Serif") +
   draw_label("Sibling Interaction", x = 0, y = 0.5, angle = 90, vjust = 1.5, fontfamily = "CMU Serif")
-plot2_1
+Figure2
 
-plot2_2 <- ggdraw() +
+Figure3 <- ggdraw() +
   draw_plot(plot2_int_2, x = 0.05, y = 0.05, width = 0.95, height = 0.95) +
   draw_label("Cultural Transmission", x = 0.5, y = 0, vjust = -0.5, fontfamily = "CMU Serif") +
   draw_label("Sibling Interaction", x = 0, y = 0.5, angle = 90, vjust = 1.5, fontfamily = "CMU Serif")
-plot2_2
-
-plot2 <- ggdraw() +
-  draw_plot(plot2_int, x = 0.05, y = 0.05, width = 0.95, height = 0.95) +
-  draw_label("Cultural Transmission (CT)", x = 0.5, y = 0, vjust = -0.5, fontfamily = "CMU Serif") +
-  draw_label("Sibling Interaction (SI)", x = 0, y = 0.5, angle = 90, vjust = 1.5, fontfamily = "CMU Serif")
-plot2
+Figure3
 
 #  labs(x = "Cultural Transmission", y = "Sibling Interaction", fill = "Power")
 
@@ -130,7 +124,7 @@ axis_lines_mzdz <- mx_mzdz_data %>%
             y_start = 0, y_end = 0.7) %>%
   ungroup()
 
-plot3 <- ggplot(data = mx_mzdz_data, mapping = aes(x = Confounder, y = Power, color = Variable)) +
+Figure4 <- ggplot(data = mx_mzdz_data, mapping = aes(x = Confounder, y = Power, color = Variable)) +
   geom_line(linewidth = 0.8) +
   geom_point(size = 1.5) +
   facet_wrap(~PGS_percent) +
@@ -151,35 +145,45 @@ plot3 <- ggplot(data = mx_mzdz_data, mapping = aes(x = Confounder, y = Power, co
         axis.title = element_text(size = 13),
         axis.text.x = element_text(angle = 45, hjust = 1),
         strip.text = element_text(size = 12))
-plot3
+Figure4
 
-# Plot 4 - Type I Error Rate for Four Different Strengths of PGS Predictive Power in the MZ & DZ vs. DZ-only Sample
-p5_mx_data <- paper_power %>%
-  filter(g == 0) %>%
-  dplyr::select("b", "p5", "PGS") %>%
-  mutate(Variable = "Cultural Transmission", Sample = "DZ") %>%
-  rename(Confounder = b, Power = p5)
-
-p6_mx_data <- paper_power %>%
+# Plot 5 - Type I Error Rate for Four Different Strengths of PGS Predictive Power in the MZ & DZ vs. DZ-only Sample
+p3_mx_data <- paper_power %>%
   filter(b == 0) %>%
-  dplyr::select("g", "p6", 'PGS') %>%
+  dplyr::select("g", "p3", "PGS") %>%
+  mutate(Variable = "Cultural Transmission", Sample = "MZ & DZ") %>%
+  rename(Parameter = g, Power = p3)
+
+p4_mx_data <- paper_power %>%
+  filter(g == 0) %>%
+  dplyr::select("b", "p4", 'PGS') %>%
+  mutate(Variable = "Sibling Interaction", Sample = "MZ & DZ") %>%
+  rename(Parameter = b, Power = p4)
+
+p7_mx_data <- paper_power %>%
+  filter(b == 0) %>%
+  dplyr::select("g", "p7", "PGS") %>%
+  mutate(Variable = "Cultural Transmission", Sample = "DZ") %>%
+  rename(Parameter = g, Power = p7)
+
+p8_mx_data <- paper_power %>%
+  filter(g == 0) %>%
+  dplyr::select("b", "p8", 'PGS') %>%
   mutate(Variable = "Sibling Interaction", Sample = "DZ") %>%
-  rename(Confounder = g, Power = p6)
+  rename(Parameter = b, Power = p8)
 
-mx_dz_data <- rbind(p5_mx_data, p6_mx_data) %>%
+full_mx_data <- rbind(p3_mx_data, p4_mx_data, p7_mx_data, p8_mx_data) %>%
   mutate(PGS_percent = factor(scales::percent(PGS), levels = c("2%", "5%", "10%", "15%")))
-
-full_mx_data <- rbind(mx_mzdz_data, mx_dz_data)
 
 axis_lines_pgs <- full_mx_data %>%
   group_by(PGS_percent) %>%
-  summarise(x_start = 0, x_end = max(Confounder),
-    y_start = 0, y_end = 0.75) %>%
+  summarise(x_start = 0, x_end = max(Parameter),
+    y_start = 0, y_end = 0.8) %>%
   ungroup()
 
-plot4 <- ggplot(data = full_mx_data) +
-  geom_line(linewidth = 0.8, mapping = aes(x = Confounder, y = Power, color = Variable, linetype = Sample)) +
-  geom_point(size = 1.5, mapping = aes(x = Confounder, y = Power, color = Variable)) +
+Figure5 <- ggplot(data = full_mx_data) +
+  geom_line(linewidth = 0.8, mapping = aes(x = Parameter, y = Power, color = Variable, linetype = Sample)) +
+  geom_point(size = 1.5, mapping = aes(x = Parameter, y = Power, color = Variable)) +
   facet_wrap(~PGS_percent) +
   geom_segment(data = axis_lines_pgs,
                aes(x = x_start, xend = x_end, y = 0, yend = 0),
@@ -199,7 +203,7 @@ plot4 <- ggplot(data = full_mx_data) +
         axis.title = element_text(size = 13),
         axis.text.x = element_text(angle = 45, hjust = 1),
         strip.text = element_text(size = 12))
-plot4
+Figure5
 
 # Prepare sample data
 sample_data <- sample_power %>%
@@ -233,7 +237,7 @@ sample_data_long <- sample_data_long %>%
   filter(`Sample Size` %in% common_sample_sizes) %>%
   filter(`Sample Size` %in% c(10000, 20000, 30000, 40000, 50000))
 
-plot6 <- ggplot(sample_data_long, aes(x = `Sample Size`, y = value,
+Figure6 <- ggplot(sample_data_long, aes(x = `Sample Size`, y = value,
                              color = variable, shape = variable)) +
   geom_line() +
   geom_point() +
@@ -265,7 +269,33 @@ plot6 <- ggplot(sample_data_long, aes(x = `Sample Size`, y = value,
    legend.text = element_text(size = 11),
    legend.title = element_text(size = 13)
   )
-plot6
+Figure6
+
+# Export
+# Save all plots as .jpgs
+jpeg("Figure2.jpg", width = 6, height = 4, units = "in", res = 600)
+print(plot2)
+dev.off()
+
+jpeg("Figure2_1.jpg", width = 6, height = 2.5, units = "in", res = 600)
+print(plot2_1)
+dev.off()
+
+jpeg("Figure2_2.jpg", width = 6, height = 2.5, units = "in", res = 600)
+print(plot2_2)
+dev.off()
+
+jpeg("Figure3.jpg", width = 8, height = 5, units = "in", res = 600)
+print(plot3)
+dev.off()
+
+jpeg("Figure4.jpg", width = 8, height = 5, units = "in", res = 300)
+print(plot4)
+dev.off()
+
+jpeg("Figure6.jpg", width = 8, height = 4, units = "in", res = 300)
+print(plot6)
+dev.off()
 
 # APPENDIX
 ext_power <- read.csv("2024-06-04_mx_power_ext.csv")
